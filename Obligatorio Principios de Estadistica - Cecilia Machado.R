bd3b0d24a44cbeab4e1016bac01b9e84770d3f4b
ceciliamachado <cecimachado8@gmail.com>
2022-02-23 19:25:05 -0300

    Add images folder

--- Obligatorio Principios de Estadistica - Cecilia Machado.R
+++ Obligatorio Principios de Estadistica - Cecilia Machado.R
@@ -1,36 +1,38 @@
   # Universidad ORT Uruguay
-  # Facultad de Administración y Ciencias Sociales
-  # Obligatorio de Principios de Estadística
-  # Cecilia Machado - N°213640
+  # Facultad de Administracion y Ciencias Sociales
+  # Obligatorio de Principios de Estadistica
+  # Cecilia Machado
 
-  # Paquete de datos requeridos: 
+  # Required packages: 
   # ISLR
 
-  # Base de datos:
+  # Database:
   # Credit
 
-  # Inicio del preámbulo:
+  #***************
+  # Initalization:
+  #***************
 
-  # Borrar todas las variables de la memoria de trabajo:
+  # Delete all variables from memory:
   rm(list=ls())
   
-  # Establecer carpeta de trabajo:
-  setwd("C:/Users/Cecilia/Desktop/ORT/Semestre 2/Principios de Estadística/Obligatorio")
+  # Set up working directory:
+  setwd("C:/Users/Cecilia Machado/Documents/GitHub/credit")
   
-  # Cargar el paquete de datos "ISLR"
+  # Load data from package "ISLR"
   library(ISLR)
   
-  # Cargar base de datos:
+  # Load database:
   data("Credit")
   
-  # Visualizar base de datos:
+  # View database:
   View(Credit)
   
   # Visualizar tipo de datos:
   str(Credit)
   head(Credit)
   
-  # Fin del preámbulo 
+  # Fin del preï¿½mbulo 
   
   # Se pide 4a:
   
@@ -69,37 +71,37 @@
   Frecuencias <- cbind(FrecAbs, RelCredit, PorCredit, Frec_Abs_Acum, Frec_Rel_Acum, Frec_Por_Acum)
   Frecuencias
   
-  # Confección de gráficos:
+  # Confecciï¿½n de grï¿½ficos:
   
-  # Gráfico de barras de Frecuencias Absolutas
+  # Grï¿½fico de barras de Frecuencias Absolutas
   
-  png("Gráfico de barras de Frecuencias Absolutas.png")
-  barplot(FrecAbs, main="Gráfico de barras de Frecuencias Absolutas", xlab="Ethnicity",ylab="Frecuencias",ylim=c(0,250))
+  png("Grï¿½fico de barras de Frecuencias Absolutas.png")
+  barplot(FrecAbs, main="Grï¿½fico de barras de Frecuencias Absolutas", xlab="Ethnicity",ylab="Frecuencias",ylim=c(0,250))
   dev.off()
   
-  # Gráfico circular de Frecuencias Porcentuales:
+  # Grï¿½fico circular de Frecuencias Porcentuales:
   
   png("Grafico circular de Frecuencias Porcentuales.png")
   pie(PorCredit,  main="Frecuencias Porcentuales")
   dev.off()
   
   # Se pide 4bi:
-  # La primer variable cuantitativa a estudiar será: "Income"
+  # La primer variable cuantitativa a estudiar serï¿½: "Income"
   
   # Tabla de frecuencias agrupadas por clases:
   
-  # Ver valores mínimos y máximos de la variable para calcular el ancho de clases:
+  # Ver valores mï¿½nimos y mï¿½ximos de la variable para calcular el ancho de clases:
   
   max(Credit$Income)
   min(Credit$Income)
   
   # Se calcula el ancho de clases de la siguiente manera:
   
-  # Ancho de clases = (Valor mayor - Valor menor)/Número de clases
+  # Ancho de clases = (Valor mayor - Valor menor)/Nï¿½mero de clases
   # Ancho de clases = (186.6 - 10.35)/5 => Ancho de clases = 35.25
-  # Se redondea el resultado obtenido, de lo contrario, algunas observaciones quedarían por fuera de las clases.
+  # Se redondea el resultado obtenido, de lo contrario, algunas observaciones quedarï¿½an por fuera de las clases.
   # Ancho de clases = 35.5
-  # La distribución de frecuencias será: 10.000-45.500, 45.500-81.000, 81.000-116.500, 116.500-152.000, 152.000-188.000.
+  # La distribuciï¿½n de frecuencias serï¿½: 10.000-45.500, 45.500-81.000, 81.000-116.500, 116.500-152.000, 152.000-188.000.
   
   val_ini         <- 10.000
   val_fin         <- 187.500
@@ -107,7 +109,7 @@
   clasesIncome          <- seq(val_ini,val_fin,salto)
   clasesIncome
   
-  # Se genera una variable tal que cada valor sea a qué clase pertenece cada observación de Income
+  # Se genera una variable tal que cada valor sea a quï¿½ clase pertenece cada observaciï¿½n de Income
   clases_Income  <- cut(Credit$Income, breaks = clasesIncome)
   print(clases_Income)
   
@@ -151,22 +153,22 @@
   tabla_frecuencia_clases_income$Frec_por_acum <- Frec_por_acum
   print(tabla_frecuencia_clases_income)
   
-  # La segunda variable cuantitativa a estudiar será: "Rating"
+  # La segunda variable cuantitativa a estudiar serï¿½: "Rating"
   
   # 1 - Tabla de frecuencias agrupadas por clases:
   
-  # Ver valores mínimos y máximos de la variable para calcular el ancho de clases:
+  # Ver valores mï¿½nimos y mï¿½ximos de la variable para calcular el ancho de clases:
   
   max(Credit$Rating)
   min(Credit$Rating)
   
   # Se calcula el ancho de clases de la siguiente manera:
   
-  # Ancho de clases = (Valor mayor - Valor menor)/Número de clases
+  # Ancho de clases = (Valor mayor - Valor menor)/Nï¿½mero de clases
   # Ancho de clases = (982 - 93)/5 => Ancho de clases = 177,8
-  # Se redondea el resultado obtenido, de lo contrario, algunas observaciones quedarían por fuera de las clases.
+  # Se redondea el resultado obtenido, de lo contrario, algunas observaciones quedarï¿½an por fuera de las clases.
   # Ancho de clases = 180
-  # La distribución de frecuencias será: 90-270, 270-450, 450-630, 630-810, 810-990.
+  # La distribuciï¿½n de frecuencias serï¿½: 90-270, 270-450, 450-630, 630-810, 810-990.
   
   val_ini         <- 90
   val_fin         <- 990
@@ -174,7 +176,7 @@
   clasesRating          <- seq(val_ini,val_fin,salto)
   clasesRating
   
-  # Se genera una variable tal que cada valor sea a qué clase pertenece cada observación de Rating
+  # Se genera una variable tal que cada valor sea a quï¿½ clase pertenece cada observaciï¿½n de Rating
   clases_Rating  <- cut(Credit$Rating, breaks = clasesRating)
   print(clases_Rating)
   
@@ -264,7 +266,7 @@
   moda               <- getmode(Credit$Rating)
   moda
   
-  # Se calcula las medidas de separación (cuartiles y deciles) de Income y Rating respectivamente
+  # Se calcula las medidas de separaciï¿½n (cuartiles y deciles) de Income y Rating respectivamente
   
   # Cuartil de Income
   
@@ -291,10 +293,10 @@
   deciles_Rating 
   
   
-  # Se calculan las medidas de disperción (rango o recorrido, Rango intercuartílico, Varianza, Desviación estándar, 
-  # coeficiente de variación) de Income y Rating respectivamente.
+  # Se calculan las medidas de disperciï¿½n (rango o recorrido, Rango intercuartï¿½lico, Varianza, Desviaciï¿½n estï¿½ndar, 
+  # coeficiente de variaciï¿½n) de Income y Rating respectivamente.
   
-  # Medidas de disperción de Income
+  # Medidas de disperciï¿½n de Income
   
   # Rango 
   
@@ -302,7 +304,7 @@
   Rango_Income       <- 186.634-10.354
   Rango_Income
   
-  # Rango intercuartílico (Q3 - Q1)
+  # Rango intercuartï¿½lico (Q3 - Q1)
   
   IQR_Income                <- IQR(Credit$Income)
   IQR_Income 
@@ -312,18 +314,18 @@
   varianza_Income       <- var(Credit$Income)
   varianza_Income
   
-  # Desviación estándar
+  # Desviaciï¿½n estï¿½ndar
   
   sd_Income                 <-sd(Credit$Income)
   sd_Income
   
   
-  # Coeficiente de variación
+  # Coeficiente de variaciï¿½n
   
   cv_Income                 <- sd_Income/media_Income
   cv_Income
   
-  # Medidas de disperción de Rating
+  # Medidas de disperciï¿½n de Rating
   
   # Rango 
   
@@ -331,7 +333,7 @@
   Rango_Rating       <- 982-93
   Rango_Rating
   
-  # Rango intercuartílico = Q3 - Q1
+  # Rango intercuartï¿½lico = Q3 - Q1
   
   IQR_Rating                <- IQR(Credit$Rating)
   IQR_Rating 
@@ -341,12 +343,12 @@
   varianza_Rating       <- var(Credit$Rating)
   varianza_Rating
   
-  # Desviación estándar
+  # Desviaciï¿½n estï¿½ndar
   
   sd_Rating                 <-sd(Credit$Rating)
   sd_Rating
   
-  # Coeficiente de variación
+  # Coeficiente de variaciï¿½n
   
   cv_Rating                 <- sd_Rating/media_Rating
   cv_Rating
@@ -378,7 +380,7 @@
   
   #Parte 4b IV:
   
-  # Detección de observaciones atípicas:
+  # Detecciï¿½n de observaciones atï¿½picas:
   
   # Para la variable Income:
   
@@ -390,7 +392,7 @@
   Q3_Income <- 57.47
   Q3_Income
   
-  # Se utiliza nuevamente el Rango intercuartílico:
+  # Se utiliza nuevamente el Rango intercuartï¿½lico:
   
   IQR_Income                <- IQR(Credit$Income)
   IQR_Income 
@@ -400,24 +402,24 @@
   LimInf_Income <- Q1_Income-1.5*IQR_Income
   LimInf_Income
   
-  # El valor obtenido es de signo negativo, por lo cual, éste se descarta debido a que no existen números
+  # El valor obtenido es de signo negativo, por lo cual, ï¿½ste se descarta debido a que no existen nï¿½meros
   # negativos dentro de la variable Income. 
-  # Igualmente se continua trabajando con el límite superior. 
+  # Igualmente se continua trabajando con el lï¿½mite superior. 
   
   LimSup_Income <- Q3_Income+1.5*IQR_Income
   LimSup_Income
   
-  # Se obtienen los índices que están arriba del límite superior:
+  # Se obtienen los ï¿½ndices que estï¿½n arriba del lï¿½mite superior:
   
   OutliersSup_Income <- which(Credit$Income>LimSup_Income)
   OutliersSup_Income
   
-  # Se ultiliza la siguiente función para mostrar los valores de los datos atípicos:
+  # Se ultiliza la siguiente funciï¿½n para mostrar los valores de los datos atï¿½picos:
   
   boxplot.stats(Credit$Income)[4]
   
-  # El número de elementos de OutliersSup_Income se corresponde 
-  # con el número de outliers del límite superior.
+  # El nï¿½mero de elementos de OutliersSup_Income se corresponde 
+  # con el nï¿½mero de outliers del lï¿½mite superior.
   
   length(OutliersSup_Income)
   
@@ -432,7 +434,7 @@
   Q3_Rating <- 437.2
   Q3_Rating
   
-  # Se utiliza nuevamente el Rango intercuartílico:
+  # Se utiliza nuevamente el Rango intercuartï¿½lico:
   
   IQR_Rating                <- IQR(Credit$Rating)
   IQR_Rating 
@@ -442,24 +444,24 @@
   LimInf_Rating <- Q1_Rating-1.5*IQR_Rating
   LimInf_Rating
   
-  # El valor obtenido es de signo negativo, por lo cual, éste se descarta debido a que no existen números
+  # El valor obtenido es de signo negativo, por lo cual, ï¿½ste se descarta debido a que no existen nï¿½meros
   # negativos dentro de la variable Income. 
-  # Igualmente se continua trabajando con el límite superior. 
+  # Igualmente se continua trabajando con el lï¿½mite superior. 
   
   LimSup_Rating <- Q3_Rating+1.5*IQR_Rating
   LimSup_Rating
   
-  # Se obtienen los datos que están arriba de los límites superiores e inferiores:
+  # Se obtienen los datos que estï¿½n arriba de los lï¿½mites superiores e inferiores:
   
   OutliersSup_Rating <- which(Credit$Rating>LimSup_Rating)
   OutliersSup_Rating
   
-  # Se ultiliza la siguiente función para mostrar los valores de los datos atípicos:
+  # Se ultiliza la siguiente funciï¿½n para mostrar los valores de los datos atï¿½picos:
   
   boxplot.stats(Credit$Rating)[4]
   
-  # El número de elementos de OutliersSup_Income se corresponde 
-  # con el número de outliers del límite superior.
+  # El nï¿½mero de elementos de OutliersSup_Income se corresponde 
+  # con el nï¿½mero de outliers del lï¿½mite superior.
   
   length(OutliersSup_Rating)
 
@@ -481,9 +483,9 @@
   
   boxplot(Credit$Rating ~ Credit$Ethnicity, data = Credit, "Rating", main = "Diagrama de cajas Rating/Ethnicity", xlab="Ethnicity", ylab="Rating")
 
-  # Se pide 6: Análisis de asociación entre Income y Ethnicity
+  # Se pide 6: Anï¿½lisis de asociaciï¿½n entre Income y Ethnicity
   
-  # Se crea tabulación cruzada de datos:
+  # Se crea tabulaciï¿½n cruzada de datos:
   
   tablaEhtnicityInome <- table(Credit$Ethnicity, clases_Income)
   tablaEhtnicityInome
@@ -492,7 +494,7 @@
   tablaEhtnicityInome <- addmargins(tablaEhtnicityInome)  
   tablaEhtnicityInome
   
-  # Se elabora distribución de frecuencia porcentual
+  # Se elabora distribuciï¿½n de frecuencia porcentual
   
   options("digits"=1)               #defino un decimal 
   
@@ -514,7 +516,7 @@
   colnames(frec_income) = "Frec. Porcentual"
   frec_income
   
-  # Cálculo de Chi Cuadrado e índice de Cramer
+  # Cï¿½lculo de Chi Cuadrado e ï¿½ndice de Cramer
   
   tablaEhtnicityInome = tablaEhtnicityInome[-4,-6]
   tablaEhtnicityInome
@@ -531,12 +533,12 @@
   cramer
   
  
-  # Se pide 7: Una comparación de las distribuciones de las dos variables 
-  # cuantitativas seleccionadas mediante un diagrama de dispersión
+  # Se pide 7: Una comparaciï¿½n de las distribuciones de las dos variables 
+  # cuantitativas seleccionadas mediante un diagrama de dispersiï¿½n
   
   
   plot(Credit$Income, Credit$Rating, xlab="Income", ylab="Rating", 
-       main="Diagrama de dispersión") 
+       main="Diagrama de dispersiï¿½n") 
   
   
   # agrego la recta de regresion al diagrama de dispersion
@@ -548,16 +550,16 @@
   # Calculo de la covarianza
   cov(Credit$Income, Credit$Rating)
   
-  # Calculamos coeficiente de correlación lineal de Pearson entre las variables
+  # Calculamos coeficiente de correlaciï¿½n lineal de Pearson entre las variables
   cor(Credit$Income, Credit$Rating)
   
-  # Comprobamos que hay correlación positiva y por lo tanto las variables están relacionadas
+  # Comprobamos que hay correlaciï¿½n positiva y por lo tanto las variables estï¿½n relacionadas
   
   
   # BONUS
   
   # La variable cualitativa es Ethnicity
-  # Se pide un análisis de la distribución por grupos de cada variable cuantitativa 
+  # Se pide un anï¿½lisis de la distribuciï¿½n por grupos de cada variable cuantitativa 
   # por clases de la variable cualitativa.
 
   
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# credit
- Analysis of Credit data obtained from the ISLR package
+# Analysis of Credit data obtained from the ISLR package
+
+## 1 – Descripción de la base de datos
+
+La base de datos utilizada en el siguiente informe fue obtenida del paquete ISLR y se llama
+Credit. 
+La misma contiene información de balances de crédito de las tarjetas de 400 clientes. 
+
+El objetivo del set de datos es predecir que clientes no van a pagar su deuda.
+
+La base de datos contiene 400 elementos y tiene 11 variables que son:
+
+* `Income`: ingreso en decenas de miles de dólares.
+
+* `Limit`: límite de crédito.
+
+* `Rating`: puntaje de crédito.
+
+* `Cards`: número de tarjetas de crédito.
+
+* `Age`: edad en años.
+
+* `Education`: número de años de educación.
+
+* `Gender`: Masculino o Femenino.
+
+* `Student`: Si o No dependiendo si la persona fue estudiante.
+
+* `Married`: Si o No dependiendo si la persona estuvo casada.
+
+* `Ethnicity`: indicador de la etnia de la persona (Afro-Americano, Caucásico o Asiático).
+
+* `Balance`: Promedio del balance de la tarjeta de crédito en dólares
+
+```r
+def hello(name):
+  print "Hello " + name
+```
+
+![](images/caja_income.png)
